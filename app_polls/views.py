d91cb3033002f2ecd261431b91173279c338b716
haiduk <hwlee91@gmail.com>
2020-10-29 00:52:01 -0400

    finished writing some tests for question model

--- app_polls/views.py
+++ app_polls/views.py
@@ -1,8 +1,11 @@
+from django.utils import timezone
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
@@ -13,11 +16,15 @@ class IndexView(generic.ListView):
 
   def get_queryset(self):
     # returns last 5 published questions
-    return Question.objects.order_by('-pub_date')[:5]
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
   model = Question
   template_name = 'app_polls/detail.html'
+
+  def get_queryset(self):
+    # excludes any questions that aren't published yet
+    return Questions.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
   model = Question
